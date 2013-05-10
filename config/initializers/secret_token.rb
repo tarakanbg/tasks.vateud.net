6ce9a2bc8d18b39f819fe3684c638a13a8a484ca
Svilen Vassilev <svilen@rubystudio.net>
2013-05-10 18:02:51 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TasksVateudNet::Application.config.secret_token = 'daa759c5f95216b7b2a7a46d085aed75b16f52d8dc25448a83599cfb74420f6c2605c488b9a0775c8ffa2551433a26fe6783e5f1666ed36665bbda30464eb43b'
